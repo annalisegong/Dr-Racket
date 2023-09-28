6772d7c9e6d7566229bc00db6eca048d25c6a3ab
annalisegong <annalise.gong@cuw.edu>
2023-09-28 09:26:34 -0500

    end of week 5
    improved processor, resolve, resolve_scope and added append_var

--- week 5.rkt
+++ week 5.rkt
@@ -1,17 +1,46 @@
 #lang racket
-(define variable_env '((a 1) (b 2) (c 33)))
-(define resolve (lambda (variable_name variable_env)
+(define variable_env '(((a 1) (b 2) (c 33) (d "string") (e a))))
+
+(define resolve_scope (lambda (variable_name scope)
     (cond
-      ((null? variable_env) (print "error: no element in variable environment"))
-      ((eq? (car (car variable_env)) variable_name) (car (cdr (car variable_env))))
-      (else (resolve variable_name (cdr variable_env)))
+      ((null? scope) null)
+      ((eq? (car (car scope)) variable_name) (car (cdr (car scope))))
+      (else (resolve_scope variable_name (cdr scope)))
       )
     )
 )
 
-(resolve 'a variable_env)
-(resolve ''a variable_env)
+(define resolve (lambda (variable_name)
+                  (if
+                    (null? variable_env) (print "error: variable not found in env")
+                    (if (null? (resolve_scope variable_name (car variable_env)))
+                        (resolve variable_name (cdr variable_env))
+                        (resolve_scope variable_name (car variable_env))
+                    )
+                  )
+                )
+  )
 
+(define append_var (lambda (variable_name variable_value)
+                     (set! variable_env
+                           (cons
+                            (cons (list variable_name variable_value) (car variable_env))
+                            (cdr variable_env)
+                            )
+                           )
+                     )
+)
+
+;(define push_scope (lambda ()
+                     ;(return cons)
+                      ;)
+  ;)
+
+;(define pop_scope (lambda ()
+                    ;(;return cdr)
+                     ; )
+;  )
+; Parser - changes the input code into a format that is easier to process
 ;(parser 'a) -> var-exp a
 ;(pasrser '(function (x) x) -> (func-exp ((var-exp x)) (var-exp x)))
 ;(parser (call (function (x) x) a)) -> (app-exp ((func-exp (var-exp x)) (var-exp x)) (var-exp a))
@@ -36,21 +65,22 @@
                        )
                       )
   )
-(parser '(function (x) x))
 
+;processor finds the variable, then bounds the identifier to the resolved variable value
 ;processor (var-exp a)) -> (resolve a variable_env -> 1
 (define processor (lambda (parse variable_env)
                     (cond
                       ((null? parse) (print "processing failed. bad parsing syntax"))
-                      ((eq? (car parse) 'var-exp) resolve (cadr parse) variable_env))
-                      (print "processing failed. unknown issue")
+                      ((eq? (car parse) 'var-exp) (resolve (cadr parse) variable_env))
+                      ((eq? (car parse) 'app-exp) (print "more to do"))
+                      (else "processing failed. unknown issue")
                     )
-  )
-(processor (parser 'a) variable_env)
+  ))
 
 (define executor (lambda (code)
                    (processor (parser code) variable_env)
                    ))
-(executor 'a)
-          
-                                      
+
+(print variable_env)
+(append_var 'hunter 20)
+(print variable_env)                                    
